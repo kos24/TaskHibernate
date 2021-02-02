6ad864fcf1c6568c415997f8acc83bd2784ac0f7
Konstantin <rebrin24@gmail.com>
2021-02-02 20:32:18 +0500

    HibernateImpl ready for testing

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -50,7 +50,6 @@ public class Util {
                 properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 properties.put(Environment.SHOW_SQL, "true");
                 properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//                properties.put(Environment.HBM2DDL_AUTO, "create-drop");
 
                 configuration.setProperties(properties);
                 configuration.addAnnotatedClass(User.class);
